9dcf3a992d581aeeb653f85bbab163aabe3f5351
richard06 <macabusrichard06@gmail.com>
2024-08-03 17:51:55 +0800

    removing the object and div whenver click the remove button

--- script.js
+++ script.js
@@ -16,7 +16,64 @@ function Book(title, author, page, read) {
   this.read = read;
 }
 
-const latestBook = myLibrary.forEach((latestBook, index) => {
+
+// const removeBtn = document.querySelector('.remove-btn');
+
+Book.prototype.displayCard = function () {
+	const mainContainer = document.querySelector('.main-section');
+	
+	const bookCardDiv = document.createElement('div');
+	bookCardDiv.classList.add('book-card');
+	
+	const bookTitle = document.createElement('h5');
+	bookTitle.textContent = `title: ${this.title}`;
+	
+	const bookAuthor = document.createElement('h5');
+	bookAuthor.textContent = `author: ${this.author}`;
+	
+	const bookPages = document.createElement('h5');
+	bookPages.textContent = `pages: ${this.page}`;
+	
+	const bookRead = document.createElement('h5');
+	bookRead.textContent = `read: ${this.read}`;
+	
+	mainContainer.appendChild(bookCardDiv);
+	bookCardDiv.appendChild(bookTitle);
+	bookCardDiv.appendChild(bookAuthor);
+	bookCardDiv.appendChild(bookPages);
+	bookCardDiv.appendChild(bookRead);
+	
+	const removeBtn = document.createElement('button');
+	removeBtn.textContent = "remove book";
+	removeBtn.classList.add('remove-btn');
+	
+	bookCardDiv.appendChild(removeBtn);
+	
+	const latestBook = myLibrary[myLibrary.length - 1]
+
+	removeBtn.addEventListener('click', () => {
+		if(latestBook.title) {
+			myLibrary.splice(myLibrary.length - 1, 1);
+			bookCardDiv.remove();
+			console.log(latestBook.title)
+		}
+	});
+};
+
+const addBookToLibrary = function (e) {
+	e.preventDefault();
+	let title = document.getElementById('title').value;
+	let author = document.getElementById('author').value;
+	let pages = document.getElementById('pages').value;
+	let read = document.getElementById('read').checked ? "read allready" : "not yet";
+	
+	let newBook = new Book(title, author, pages, read);
+	myLibrary.push(newBook);
+	newBook.displayCard();
+	document.forms[0].reset();
+};
+
+const latestBook = myLibrary.forEach((latestBook, index, array) => {
 	const mainContainer = document.querySelector('.main-section');
 	
 	const bookCardDiv = document.createElement('div');
@@ -45,64 +102,15 @@ const latestBook = myLibrary.forEach((latestBook, index) => {
 	removeBtn.classList.add('remove-btn');
 	
 	bookCardDiv.appendChild(removeBtn);
-	
-	removeBtn.addEventListener('click', (i) => {
-		console.log(myLibrary[index], i.target);
-		myLibrary.splice(index, 1);
+	removeBtn.addEventListener('click', () => {
+		if(latestBook) {
+			console.log(latestBook.title, index)
+			myLibrary.splice(index, 1);
+			bookCardDiv.remove();
+		}
 	});
 });
-	// const removeBtn = document.querySelector('.remove-btn');
-	
-Book.prototype.displayCard = function () {
-	const mainContainer = document.querySelector('.main-section');
-	
-	const bookCardDiv = document.createElement('div');
-	bookCardDiv.classList.add('book-card');
-	
-	const bookTitle = document.createElement('h5');
-	bookTitle.textContent = `title: ${this.title}`;
-	
-	const bookAuthor = document.createElement('h5');
-	bookAuthor.textContent = `author: ${this.author}`;
-	
-	const bookPages = document.createElement('h5');
-	bookPages.textContent = `pages: ${this.page}`;
-	
-	const bookRead = document.createElement('h5');
-	bookRead.textContent = `read: ${this.read}`;
-	
-	mainContainer.appendChild(bookCardDiv);
-	bookCardDiv.appendChild(bookTitle);
-	bookCardDiv.appendChild(bookAuthor);
-	bookCardDiv.appendChild(bookPages);
-	bookCardDiv.appendChild(bookRead);
-
-	const removeBtn = document.createElement('button');
-	removeBtn.textContent = "remove book";
-	removeBtn.classList.add('remove-btn');
-	
-	bookCardDiv.appendChild(removeBtn);
-	
-	removeBtn.addEventListener('click', (i) => {
-		if(myLibrary[index] === i.target) {
-			// console.log("jello");
-		}
-		// myLibrary.map.splice(index, 1);
-	});
-};
-
-const addBookToLibrary = function (e) {
-	e.preventDefault();
-	let title = document.getElementById('title').value;
-	let author = document.getElementById('author').value;
-	let pages = document.getElementById('pages').value;
-	let read = document.getElementById('read').checked ? "read allready" : "not yet";
-	
-	let newBook = new Book(title, author, pages, read);
-	myLibrary.push(newBook);
-	newBook.displayCard();
-	document.forms[0].reset();
-};
 
 const button = document.querySelector('.btn');
 button.addEventListener('click', addBookToLibrary);
+console.log(myLibrary)
